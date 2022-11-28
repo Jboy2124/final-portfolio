d64f2d5f5739ddff505f7d240c0548b20e4126b8
Jboy Abecia <jufel.abecia24@gmail.com>
2022-11-28 10:52:31 +0800

    feat: adding slider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import React from 'react'
 import MainPage from './pages/MainPage'
+
+
 
 const App = () => {
   return (
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,32 +1,82 @@
 import React from 'react'
 import WorkExpCard from '../cards/WorkExpCard'
 import { workExp } from '../utils/work-utils'
+import Slider from 'react-slick'
+
 
 const WorkExperience = () => {
+
+  const settings = {
+      dots: true,
+      infinite: false,
+      autoplay: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 1440,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow:2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  }
+
   return (
-    <div name='work-experience' className='min-h-screen'>
-    <div className='container mx-auto'>
-        <div className='min-h-[90vh] flex flex-col items-center'>
-                <div className='min-h-[8vh] flex justify-center items-center pt-10'>
-                    <span className='text-[20px] text-[#5C3D2E] font-poppins font-bold'>WORK EXPERIENCES</span>
-                </div>
-                <div className='grid mobile:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4 place-items-center gap-5 mx-10 pt-10'>
-                  {workExp.map(items => {
-                    return(
-                      <div key={items.id}>
-                          <WorkExpCard 
-                              image={items.image}
-                              company={items.company}
-                              position={items.position}
-                              employment={items.employment}
-                          />
-                      </div>
-                    )
-                  })}
-                </div>
+    <div name='work-experience' className='min-h-screen'>       
+        <div className='container mx-auto'>
+          <div className='min-h-[80vh]'>
+              <div className='py-40'>
+              <h2 className='text-center py-10 text-[20px] text-[#5C3D2E] font-poppins font-bold'>WORK EXPERIENCE</h2>
+                <Slider {...settings}>
+                    {workExp.map(items => {
+                        return(
+                            <div key={items.id} className='py-5'>
+                                <WorkExpCard 
+                                    image={items.image}
+                                    company={items.company}
+                                    position={items.position}
+                                    employment={items.employment}
+                                          />
+                            </div>
+                        )
+                      })}
+                  </Slider>
+              </div>
+          </div>
         </div>
     </div>
-</div>
+    
   )
 }
 
